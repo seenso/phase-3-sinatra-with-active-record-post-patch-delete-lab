7e6e9ee4fbb91c0973911e68447fc61a049f4db7
seenso <>
2021-12-31 10:56:01 -0900

    commit tree 7daf81b6ba310abcadb3dca0f075905c5d5f5ee0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < Sinatra::Base
   set default_content_type: "application/json"
   
+  #GET
   get '/bakeries' do
     bakeries = Bakery.all
     bakeries.to_json
@@ -20,6 +21,40 @@ class ApplicationController < Sinatra::Base
   get '/baked_goods/most_expensive' do
     # see the BakedGood class for the  method definition of `.by_price`
     baked_good = BakedGood.by_price.first
+    baked_good.to_json
+  end
+
+  #POST
+  post '/baked_goods' do
+    #create
+    baked_good = BakedGood.create(
+      name: params[:name],
+      price: params[:price],
+      bakery_id: params[:bakery_id]
+    )
+    #return json response
+    baked_good.to_json
+  end
+
+  #PATCH
+  patch '/bakeries/:id' do
+    #find bakery
+    bakery = Bakery.all.find(params[:id])
+    #.update() bakery
+    bakery.update(
+      name: params[:name]
+    )
+    #return response with updated bakery
+    bakery.to_json
+  end
+
+  #DELETE
+  delete '/baked_goods/:id' do
+    #find
+    baked_good = BakedGood.all.find(params[:id])
+    #destroy
+    baked_good.destroy
+    #send response with deleted bakery
     baked_good.to_json
   end
 
